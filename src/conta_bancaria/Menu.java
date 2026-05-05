75ad00ce5d90fa78d59470ed85c409f5aed9cfa5
Monique Carvalho <moniquecarvalho654@gmail.com>
2026-05-05 15:31:31 -0300

    Finalizando a ContaRepository e a Exceptions

--- src/conta_bancaria/Menu.java
+++ src/conta_bancaria/Menu.java
@@ -1,5 +1,6 @@
 package conta_bancaria;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import conta_bancaria.model.ContaCorrente;
@@ -13,22 +14,6 @@ public class Menu {
 	public static void main(String[] args) {
 
 		int opcao;
-
-		// Teste da Classe Conta Corrente
-		ContaCorrente cc1 = new ContaCorrente(2, 123, 1, "José da Silva", 0.0f, 1000.0f);
-		cc1.visualizar();
-		cc1.sacar(12000.0f);
-		cc1.visualizar();
-		cc1.depositar(5000.0f);
-		cc1.visualizar();
-
-		// Teste da Classe Conta Poupança
-		ContaPoupanca cp1 = new ContaPoupanca(3, 123, 2, "Maria dos Santos", 100000.0f, 15);
-		cp1.visualizar();
-		cp1.sacar(1000.0f);
-		cp1.visualizar();
-		cp1.depositar(5000.0f);
-		cp1.visualizar();
 
 		while (true) {
 
@@ -51,11 +36,19 @@ public class Menu {
 			System.out.println("                                                     ");
 			System.out.println("*****************************************************");
 			System.out.println("Entre com a opção desejada:                          ");
-			System.out.println("                                                     ");
-			opcao = leia.nextInt();
+			System.out.println("                                                     " + Cores.TEXT_RESET);
+
+			try {
+				opcao = leia.nextInt();
+				leia.nextLine();
+			} catch (InputMismatchException e) {
+				opcao = -1;
+				System.out.println("\nDigite um número inteiro!");
+				leia.nextLine();
+			}
 
 			if (opcao == 0) {
-				System.out.println("\nBanco do Brazil com Z - O seu futuro começa aqui!");
+				System.out.println(Cores.TEXT_WHITE_BOLD + "\nBanco do Brazil com Z - O seu futuro começa aqui!");
 				sobre();
 				leia.close();
 				System.exit(0);
@@ -63,51 +56,67 @@ public class Menu {
 
 			switch (opcao) {
 			case 1:
-				System.out.println("\n Criar Conta");
+				System.out.println(Cores.TEXT_WHITE + "Criar Conta\n\n");
 
+				keyPress();
 				break;
 			case 2:
-				System.out.println("\n Listar todas as Contas");
+				System.out.println(Cores.TEXT_WHITE + "Listar todas as Contas\n\n");
 
+				keyPress();
 				break;
 			case 3:
-				System.out.println("\n Buscar Conta por número");
+				System.out.println(Cores.TEXT_WHITE + "Consultar dados da Conta - por número\n\n");
 
+				keyPress();
 				break;
 			case 4:
-				System.out.println("\n Atualizar dados da Conta");
+				System.out.println(Cores.TEXT_WHITE + "Atualizar dados da Conta\n\n");
 
+				keyPress();
 				break;
 			case 5:
-				System.out.println("\n Apagar Conta");
+				System.out.println(Cores.TEXT_WHITE + "Apagar a Conta\n\n");
 
+				keyPress();
 				break;
 			case 6:
-				System.out.println("\n Sacar");
+				System.out.println(Cores.TEXT_WHITE + "Saque\n\n");
 
+				keyPress();
 				break;
 			case 7:
-				System.out.println("\n Depositar");
+				System.out.println(Cores.TEXT_WHITE + "Depósito\n\n");
 
+				keyPress();
 				break;
 			case 8:
-				System.out.println("\n Transferir");
+				System.out.println(Cores.TEXT_WHITE + "Transferência entre Contas\n\n");
 
+				keyPress();
 				break;
 			default:
-				System.out.println("\nOpção Inválida");
+				System.out.println(Cores.TEXT_RED_BOLD + "\nOpção Inválida!\n" + Cores.TEXT_RESET);
+				keyPress();
 				break;
 			}
 		}
+
 	}
 
 	public static void sobre() {
-		System.out.println(Cores.TEXT_YELLOW + Cores.ANSI_BLACK_BACKGROUND
-				+ "\n**************************************************************");
+		System.out.println("\n**************************************************************");
 		System.out.println("Projeto Desenvolvido por: ");
 		System.out.println("Monique Carvalho - moniquecarvalho654@gmail.com ");
 		System.out.println("https://github.com/Moniquecarvalho");
 		System.out.println("****************************************************************");
 
 	}
+
+	private static void keyPress() {
+		System.out.println(Cores.TEXT_RESET + "\n\nPressione Enter para Continuar...");
+		leia.nextLine();
+
+	}
+
 }
